4546ff2e7758c8c4e11884a4d0febdde1721198c
Arya Sukresno <aryasukresno10@gmail.com>
2024-12-30 15:02:30 +0000

    submit

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,55 +4,86 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+bmi = df['weight'] / ((df['height']/100) ** 2)
+df['overweight'] = (bmi > 25).astype(int)
 
 # 3
-
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(
+        df, 
+        id_vars=['cardio'], 
+        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],
+        var_name='variable', 
+        value_name='value'
+    )
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
-
+    fig = sns.catplot(
+        data=df_cat,
+        x='variable', 
+        y='total', 
+        hue='value', 
+        col='cardio', 
+        kind='bar', 
+        height=5, 
+        aspect=1
+    ).fig
 
 
     # 8
-    fig = None
+    
 
 
     # 9
     fig.savefig('catplot.png')
     return fig
 
+draw_cat_plot()
+
 
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
-
-
-    # 14
-    fig, ax = None
+      # 14
+    fig, ax = plt.subplots(figsize=(12, 8))
 
     # 15
-
+    sns.heatmap(
+        corr, 
+        mask=mask, 
+        annot=True, 
+        fmt=".1f", 
+        cmap="coolwarm", 
+        square=True, 
+        cbar_kws={"shrink": 0.5},
+        ax=ax
+    )
 
 
     # 16
